fd64aba57795891e9619e23f5ba4db4f77d604b7
Sidharth Hulyalkar <sid.soccer.21@gmail.com>
2026-08-18 15:56:28 -0700

    Export v0.13 drive-volume research surface

--- src/player_state_engine/game_intelligence/__init__.py
+++ src/player_state_engine/game_intelligence/__init__.py
@@ -10,6 +10,17 @@ from player_state_engine.game_intelligence.blend import (
     QuantileBlendCalibrator,
     expanding_quantile_blend_benchmark,
 )
+from player_state_engine.game_intelligence.drive import (
+    DriveVolumeModel,
+    evaluate_drive_volume_draws,
+    evaluate_pace_event_scores,
+    extract_drive_frame,
+    observed_drive_volume,
+    permute_pace_targets_within_team_season,
+)
+from player_state_engine.game_intelligence.drive_simulator import (
+    simulate_matchup_volume_probe,
+)
 from player_state_engine.game_intelligence.factorial import (
     FactorialBenchmarkResult,
     recommend_next_development,
@@ -29,9 +40,17 @@ from player_state_engine.game_intelligence.tendencies import (
     build_team_tendency_snapshots,
 )
 from player_state_engine.game_intelligence.usage import build_player_usage_profiles
+from player_state_engine.game_intelligence.volume_benchmark import (
+    DriveVolumeBenchmarkResult,
+    recommend_v014_development,
+    run_v013_drive_volume_benchmark,
+    v013_drive_volume_promotion_gate,
+)
 
 __all__ = [
     "BlendBenchmarkResult",
+    "DriveVolumeBenchmarkResult",
+    "DriveVolumeModel",
     "EmpiricalPlayOutcomeModel",
     "ExpandingGameBenchmarkResult",
     "FactorialBenchmarkResult",
@@ -43,12 +62,21 @@ __all__ = [
     "build_play_intelligence_frame",
     "build_player_usage_profiles",
     "build_team_tendency_snapshots",
+    "evaluate_drive_volume_draws",
+    "evaluate_pace_event_scores",
     "expanding_quantile_blend_benchmark",
+    "extract_drive_frame",
+    "observed_drive_volume",
     "permute_context_within_team_season",
+    "permute_pace_targets_within_team_season",
     "recommend_next_development",
+    "recommend_v014_development",
     "run_expanding_game_benchmark",
     "run_v012_factorial_benchmark",
+    "run_v013_drive_volume_benchmark",
     "simulate_matchup",
+    "simulate_matchup_volume_probe",
     "v011_research_promotion_gate",
     "v012_state_opportunity_promotion_gate",
+    "v013_drive_volume_promotion_gate",
 ]
