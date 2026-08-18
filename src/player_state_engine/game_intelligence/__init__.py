eea4b28d22f37d7041deb8c0b69fe683614fab8f
Sidharth Hulyalkar <sid.soccer.21@gmail.com>
2026-08-18 13:41:52 -0700

    export v0.11 game intelligence research APIs

--- src/player_state_engine/game_intelligence/__init__.py
+++ src/player_state_engine/game_intelligence/__init__.py
@@ -1,6 +1,17 @@
-"""Point-in-time NFL game intelligence and play-by-play simulation."""
+"""Point-in-time NFL game intelligence, frozen replay, and play-by-play simulation."""
 
+from player_state_engine.game_intelligence.benchmark import (
+    ExpandingGameBenchmarkResult,
+    run_expanding_game_benchmark,
+    v011_research_promotion_gate,
+)
+from player_state_engine.game_intelligence.blend import (
+    BlendBenchmarkResult,
+    QuantileBlendCalibrator,
+    expanding_quantile_blend_benchmark,
+)
 from player_state_engine.game_intelligence.models import EmpiricalPlayOutcomeModel, PlayCallModel
+from player_state_engine.game_intelligence.opportunity import StateConditionedOpportunityModel
 from player_state_engine.game_intelligence.play_features import build_play_intelligence_frame
 from player_state_engine.game_intelligence.simulator import simulate_matchup
 from player_state_engine.game_intelligence.tendencies import (
@@ -11,12 +22,19 @@ from player_state_engine.game_intelligence.tendencies import (
 from player_state_engine.game_intelligence.usage import build_player_usage_profiles
 
 __all__ = [
+    "BlendBenchmarkResult",
     "EmpiricalPlayOutcomeModel",
+    "ExpandingGameBenchmarkResult",
     "PlayCallModel",
+    "QuantileBlendCalibrator",
+    "StateConditionedOpportunityModel",
     "build_coaching_matchup_history",
     "build_matchup_profile",
     "build_play_intelligence_frame",
     "build_player_usage_profiles",
     "build_team_tendency_snapshots",
+    "expanding_quantile_blend_benchmark",
+    "run_expanding_game_benchmark",
     "simulate_matchup",
+    "v011_research_promotion_gate",
 ]
